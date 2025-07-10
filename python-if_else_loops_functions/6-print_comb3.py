6f34a781008f4c2342535b56f4c2347d727c10d6
Cadeaulanuel02 <aydiin.mohamed@gmail.com>
2025-07-10 13:54:18 +0400

    change to remove the extra zero in the start

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,9 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 0 and j == 1:
-            print("{:02d}{:d}".format(i, j), end="")
+            print("{}{}".format(i, j), end="")
         elif i == 8 and j == 9:
-            print(", {:d}{:d}".format(i, j))
+            print(", {}{}".format(i, j))
         else:
-            print(", {:d}{:d}".format(i, j), end="")
+            print(", {}{}".format(i, j), end="")
+
