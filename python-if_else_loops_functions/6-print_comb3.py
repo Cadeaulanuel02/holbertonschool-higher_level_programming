f4f8cec428f73682512a5b07b2cdb24a1eb2a0c7
Cadeaulanuel02 <aydiin.mohamed@gmail.com>
2025-07-10 13:44:38 +0400

    changed to string using .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print(f"{i}{j}")
+        if i == 0 and j == 1:
+            print("{:02d}{:d}".format(i, j), end="")
+        elif i == 8 and j == 9:
+            print(", {:d}{:d}".format(i, j))
         else:
-            print(f"{i}{j}", end=", ")
+            print(", {:d}{:d}".format(i, j), end="")
+
 
 
